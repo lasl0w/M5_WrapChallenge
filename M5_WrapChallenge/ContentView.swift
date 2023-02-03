2907461cc2d1e317d447dfea4a5cc009da2aaafc
Tom Montgomery <montgomery3681@gmail.com>
2023-02-02 20:49:08 -0700

    Initial Commit

---
+++ M5_WrapChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M5_WrapChallenge
+//
+//  Created by tom montgomery on 2/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M5_WrapChallenge/M5_WrapChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5_WrapChallengeApp.swift
+//  M5_WrapChallenge
+//
+//  Created by tom montgomery on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct M5_WrapChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
